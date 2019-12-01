0b33f1e48716e972ab051a2d1c2ca39a14b052bf
Tsdy <571219188@qq.com>
2019-12-01 17:37:47 +0800

    把:v-model 修改为v-model

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -12,13 +12,13 @@
           <div class="register-box">
             <el-form ref="form" :model="form" label-width="80px" label-position="top">
               <el-form-item label="username">
-                <el-input :v-model="form.username"></el-input>
+                <el-input v-model="form.username"></el-input>
               </el-form-item>
               <el-form-item label="Email">
-                <el-input :v-model="form.email"></el-input>
+                <el-input v-model="form.email"></el-input>
               </el-form-item>
               <el-form-item label="password">
-                <el-input :v-model="form.password" show-password></el-input>
+                <el-input v-model="form.password" show-password></el-input>
               </el-form-item>
               <el-form-item>
                 <el-button class="submit" type="success" @click="onSubmit">sign up</el-button>
